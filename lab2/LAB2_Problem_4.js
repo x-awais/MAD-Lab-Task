568ae1ae7b27962737fc4997941b3a06018ef501
Awais Khan <99994684+x-awais@users.noreply.github.com>
2022-03-08 14:17:42 +0500

    Add files via upload

---
+++ lab2/LAB2_Problem_4.js
@@ -0,0 +1,11 @@
+function SandwichCalculator(Bread,cheese) {
+ if(Bread%2==0 && cheese>1)
+ {
+     return Bread/2;
+ }
+ else{
+    return "insufficient ingridients "
+ }
+  
+};
+console.log(SandwichCalculator(12),(6))
