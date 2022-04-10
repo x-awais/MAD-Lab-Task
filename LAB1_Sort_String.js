ee5dc1841abfa4ec5b1910dfbb2099e0a58547e7
Awais Khan <99994684+x-awais@users.noreply.github.com>
2022-04-10 17:23:42 +0500

    Delete LAB1_Sort_String.js

--- LAB1_Sort_String.js
+++
@@ -1,9 +0,0 @@
-// Write a JavaScript function that returns a passed string with letters in alphabetical order.
-
-String("comsats");
-function String(str) {
-    String = str.split("");
-}
-String.sort()
-Stringa = String.toString();
-console.log(Stringa)
