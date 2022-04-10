609d61458b6f780ff0cc85fcd5b38174c9729822
Awais Khan <99994684+x-awais@users.noreply.github.com>
2022-04-10 17:22:55 +0500

    Delete lab2 directory

--- lab2/LAB2_Problem_1.js
+++
@@ -1,9 +0,0 @@
-var student = {
-    name : "David Rayy",
-    sclass : "VI",
-    rollno : 12 };
-   
-    
-    if (student.hasOwnProperty("name","sclass","rollno")) {
-        console.log(student.name,student.sclass,student.rollno);
-      }
